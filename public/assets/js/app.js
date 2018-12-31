1d4ffd741552542e27e0cc503d6e639134f6f334
Jeme <aime.urquieta@outlook.com>
2018-12-31 09:12:25 -0600

    location change

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -165,6 +165,6 @@ $(document).on("click", "#loginSubmit", function() {
     data: data
   }).then(data => {
     console.log("sent data");
-    window.location.assign(`/home/${data._id}`);
+    window.location.assign(`https://medium-scraper-and-note-taker.herokuapp.com/home/${data._id}`);
   });
 });
